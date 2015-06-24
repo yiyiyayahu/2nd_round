4b0679253385c01db00439560b31a3d007137eb3
yiyiyayahu <yuanjie_qu@hotmail.com>
2015-06-23 23:15:54 -0700

    Update Balanced Binary Tree.java

--- Tree/Balanced Binary Tree.java
+++ Tree/Balanced Binary Tree.java
@@ -14,6 +14,11 @@ in which the depth of the two subtrees of every node never differ by more than 1
  *     TreeNode(int x) { val = x; }
  * }
  */
+ 
+/*
+原来我这个题用的一直是最差的解法
+时间复杂度是O(n^2) 空间是O(n) stack space(recursion).这个空间是怎么算的我要研究下。。。
+*/
 public class Solution {
     public boolean isBalanced(TreeNode root) {
         if(root == null)  return true;
