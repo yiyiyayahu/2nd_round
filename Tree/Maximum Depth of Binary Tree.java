78bf384078659110c3419f004518e0b58110c3f9
yiyiyayahu <yuanjie_qu@hotmail.com>
2015-06-23 23:44:41 -0700

    Update Maximum Depth of Binary Tree.java

--- Tree/Maximum Depth of Binary Tree.java
+++ Tree/Maximum Depth of Binary Tree.java
@@ -6,10 +6,13 @@ The maximum depth is the number of nodes along the longest path from the root no
 
 /*
 时间复杂度O(n) 空间复杂度为啥是O(logn)?
-Assume that n is the total number of nodes in the tree. The runtime complexity is O(n)
-because it traverse each node exactly once. As the maximum depth of a binary tree is
-O(log n), the extra space cost is O(log n) due to the extra stack space used by the
-recursion.
+
+recursion的空间复杂度是这样的
+比如factorial(n) = n * factorial(n-1)
+那我算f(5)的时候我会调用f(4)同时memory(stack)要把5的状态存下来，同理，f(4)调用f(3)，存f(4)的状态。。。
+然后f(1)调用完了之后就stack一点点pop出来，所以这个时候空间复杂度是O(n)
+
+那这道题呢？额，还是不咋懂。。囧
 */
 
 /**
