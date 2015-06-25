95cb3bbece38c81c71c0dc2fa6d6c3e1a89447f3
yiyiyayahu <yuanjie_qu@hotmail.com>
2015-06-24 22:50:32 -0700

    Update Symmetric Tree.java

--- Tree/Symmetric Tree.java
+++ Tree/Symmetric Tree.java
@@ -43,3 +43,44 @@ public class Solution {
         return helper(left.left, right.right) && helper(left.right, right.left);
     }
 }
+
+/*
+iterative的解法
+开始以为只要类似层递归那种就可以呢，然后检验每一层的arr是不是对称的
+后来发现，不行，有可能在某个位置是null的就不太好办了。
+不过其实也是可以做的，ArrayList<Integer>里面放null就好了，只是如果上一层是连着的两个null，下一层出现四个null有点搞笑哈。。。
+还不如用两个queue呢，一个是left的，一个是right的
+*/
+
+public class Solution {
+    public boolean isSymmetric(TreeNode root) {
+        if(root == null) return true;
+        if(root.left == null && root.right == null) return true;
+        if(root.left == null || root.right == null) return false;
+        
+        Queue<TreeNode> q1 = new LinkedList<TreeNode>();
+        Queue<TreeNode> q2 = new LinkedList<TreeNode>();
+        
+        q1.add(root.left);
+        q2.add(root.right);
+        
+        while(!q1.isEmpty() && !q2.isEmpty()) {
+            TreeNode left = q1.remove();
+            TreeNode right = q2.remove();
+            
+            if(left.val != right.val) return false;
+            if((left.left == null && right.right != null) || (left.right == null && right.left != null)) return false; 
+            if((left.left != null && right.right == null) || (left.right != null && right.left == null)) return false;
+            
+            if(left.left != null && right.right != null) {
+                q1.add(left.left);
+                q2.add(right.right);
+            }
+            if(left.right != null && right.left != null) {
+                q1.add(left.right);
+                q2.add(right.left);
+            }
+        }
+        return true;
+    }
+}
