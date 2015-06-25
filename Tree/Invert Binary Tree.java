d93601aac4d30c47f06b19a77288ccd81c01da30
yiyiyayahu <yuanjie_qu@hotmail.com>
2015-06-24 23:11:15 -0700

    Update Invert Binary Tree.java

--- Tree/Invert Binary Tree.java
+++ Tree/Invert Binary Tree.java
@@ -17,6 +17,11 @@ Google: 90% of our engineers use the software you wrote (Homebrew),
 but you can’t invert a binary tree on a whiteboard so fuck off.
 */
 
+
+/*
+Time: O(n) Space: O(logn)
+*/
+
 /**
  * Definition for a binary tree node.
  * public class TreeNode {
