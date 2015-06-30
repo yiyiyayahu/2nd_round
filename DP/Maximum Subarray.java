cffed0f9ea8843113d96cf32886b5c1bbcb5e371
yiyiyayahu <yuanjie_qu@hotmail.com>
2015-06-30 00:04:05 -0700

    Update and rename MaximumSubarray.java to Maximum Subarray.java

--- DP/Maximum Subarray.java
+++ DP/Maximum Subarray.java
@@ -11,6 +11,13 @@ If you have figured out the O(n) solution,
 try coding another solution using the divide and conquer approach, which is more subtle.
 */
 
+/*
+这种DP其实是局部最优和全局最优，（诶，感觉和tree里面那个maximum path sum差不多诶，就是可以start和end at any node，也是要maintain一个全局最优和局部最优）
+就是max来表示全局最优，result来表示局部最优，当result<0的时候发现加上之前的负值完全不起作用，就重置为0重新开始
+这个开始max不能设为0因为有可能全部是负数
+Time: O(n) Space: O(1)
+*/
+
 public class Solution {
     public int maxSubArray(int[] nums) {
         int max = Integer.MIN_VALUE;
