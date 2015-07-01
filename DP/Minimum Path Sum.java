b5e1f107098eb15f7241cd8fe1384ef3b40f668c
yiyiyayahu <yuanjie_qu@hotmail.com>
2015-06-30 23:15:45 -0700

    Update Minimum Path Sum.java

--- DP/Minimum Path Sum.java
+++ DP/Minimum Path Sum.java
@@ -4,6 +4,25 @@ find a path from top left to bottom right which minimizes the sum of all numbers
 
 Note: You can only move either down or right at any point in time.
 */
+
+/*
+改为一维滚动数组
+*/
+public class Solution {
+    public int minPathSum(int[][] grid) {
+        int m = grid.length, n = grid[0].length;
+        int[] dp = new int[n];
+        
+        for(int i = 0; i < m; i++) {
+            for(int j = 0; j < n; j++) {
+                if(i == 0 && j == 0) dp[0] = grid[0][0];
+                else dp[j] = Math.min(i>0?dp[j]:Integer.MAX_VALUE, j>0?dp[j-1]:Integer.MAX_VALUE) + grid[i][j];
+            }
+        }
+        return dp[n-1];
+    }
+}
+
 public class Solution {
     public int minPathSum(int[][] grid) {
         int m = grid.length, n = grid[0].length;
