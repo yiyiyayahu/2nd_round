71c647490a06866298f62c433d9ce38dd6e7db5f
yiyiyayahu <yuanjie_qu@hotmail.com>
2015-07-08 22:45:43 -0700

    Update Majority Element.java

--- Array-String/Majority Element.java
+++ Array-String/Majority Element.java
@@ -3,10 +3,12 @@ Given an array of size n, find the majority element. The majority element is the
 
 You may assume that the array is non-empty and the majority element always exist in the array.
 
-Credits:
-Special thanks to @ts for adding this problem and creating all test cases.
 */
 
+/*
+time: O(nlogn) space: O(1)
+肯定有O(n)的解法
+*/
 public class Solution {
     public int majorityElement(int[] nums) {
         Arrays.sort(nums);
