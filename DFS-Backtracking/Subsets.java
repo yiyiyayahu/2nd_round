fccfe82337367d19dbdda57fbd61706418ce17ef
yiyiyayahu <yuanjie_qu@hotmail.com>
2015-07-13 23:19:09 -0700

    Update Subsets.java

--- DFS-Backtracking/Subsets.java
+++ DFS-Backtracking/Subsets.java
@@ -19,6 +19,30 @@ If nums = [1,2,3], a solution is:
 ]
 */
 
+/*
+很巧妙诶，只要每次都直接add到result里面就可以啦，不用像之前那样考虑size的问题
+*/
+public class Solution {
+    public List<List<Integer>> subsets(int[] nums) {
+        List<List<Integer>> result = new ArrayList<List<Integer>>();
+        if(nums == null || nums.length == 0) return result;
+        Arrays.sort(nums);
+        result.add(new ArrayList<Integer>());
+        helper(nums, 0, new ArrayList<Integer>(), result);
+        return result;
+    }
+    public void helper(int[] nums, int curr, List<Integer> list, List<List<Integer>> result) {
+        for(int i = curr; i < nums.length; i++) {
+            list.add(nums[i]);
+            result.add(new ArrayList<Integer>(list));
+            helper(nums, i+1, list, result);
+            list.remove(list.size()-1);
+        }
+    }
+}
+
+
+
 public class Solution {
     public List<List<Integer>> subsets(int[] nums) {
         List<List<Integer>> result = new ArrayList<List<Integer>>();
