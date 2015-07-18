ee019a4ab87a150c33d87b6629e69fbb4c7e01df
yiyiyayahu <yuanjie_qu@hotmail.com>
2015-07-17 23:27:35 -0700

    Create Palindrome Linked List.java

---
+++ Palindrome/Palindrome Linked List.java
@@ -0,0 +1,36 @@
+/*
+Given a singly linked list, determine if it is a palindrome.
+
+Follow up:
+Could you do it in O(n) time and O(1) space?
+*/
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int x) { val = x; }
+ * }
+ */
+ 
+ /*
+ I. use a stack, traverse the linked list twice
+    time: O(n) space: O(n)
+ */
+public class Solution {
+    public boolean isPalindrome(ListNode head) {
+        Stack<ListNode> stack = new Stack<ListNode>();
+        ListNode tmp = head;
+        while(tmp != null) {
+            stack.push(tmp);
+            tmp = tmp.next;
+        }
+        
+        tmp = head;
+        while(tmp != null) {
+            if(tmp.val != stack.pop().val) return false;
+            tmp = tmp.next;
+        }
+        return true;
+    }
+}
