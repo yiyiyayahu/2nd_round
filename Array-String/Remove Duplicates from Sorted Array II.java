90de4f9261f3b465452a32f86a892a0d690b3b2f
yiyiyayahu <yuanjie_qu@hotmail.com>
2015-07-21 22:57:54 -0700

    Update Remove Duplicates from Sorted Array II.java

--- Array-String/Remove Duplicates from Sorted Array II.java
+++ Array-String/Remove Duplicates from Sorted Array II.java
@@ -8,6 +8,28 @@ Given sorted array nums = [1,1,1,2,2,3],
 Your function should return length = 5, with the first five elements of nums being 1, 1, 2, 2 and 3. 
 It doesn't matter what you leave beyond the new length.
 */
+
+/*
+很巧妙的做法，可以扩展成allow N duplicates
+time: O(n) space: O(1)
+*/
+public class Solution {
+    public int removeDuplicates(int[] nums) {
+        if(nums == null || nums.length == 0) return 0;
+        
+        int len = nums.length;
+        if(len <= 2) return len;
+        int index = 2, tmp = nums[0];
+        for(int i = 2; i < len; i++) {
+            if(nums[i] != nums[index-2]) {
+                nums[index++] = nums[i];
+            }
+        }
+        return index;
+    }
+}
+
+
 public class Solution {
     public int removeDuplicates(int[] nums) {
         if(nums == null || nums.length == 0) return 0;
