089f63925c931ba0b5e5e1706331d604d68e7228
yiyiyayahu <yuanjie_qu@hotmail.com>
2015-07-28 23:37:31 -0700

    Update Read N Characters Given Read4 II - Call multiple times.java

--- Array-String/Read N Characters Given Read4 II - Call multiple times.java
+++ Array-String/Read N Characters Given Read4 II - Call multiple times.java
@@ -13,7 +13,38 @@ The read function may be called multiple times.
 /*
 被call了multiple time其实就是有一些read4读在buffer里，但是没有完全取出来，这种情况要handle
 所以要定义几个变量
+leetcode那本书里面的解法，要简洁好多
+bufferSize=0就表示没有之前的留下来重新读就好了
 */
+
+public class Solution extends Reader4 {
+    /**
+     * @param buf Destination buffer
+     * @param n   Maximum number of characters to read
+     * @return    The number of characters read
+     */
+    char[] buffer = new char[4];
+    int offset = 0, bufferSize = 0;
+    
+    public int read(char[] buf, int n) {
+        if(n == 0) return 0;
+        boolean eof = false;
+        int readBytes= 0;
+
+        while(!eof && readBytes < n) {
+            int size = bufferSize > 0 ? bufferSize : read4(buffer);
+            if(bufferSize == 0 && size < 4) eof = true;
+            int bytes = Math.min(n-readBytes, size);
+            System.arraycopy(buffer, offset, buf, readBytes, bytes);
+            
+            offset = (offset+bytes)%4;
+            bufferSize = size - bytes;
+            readBytes += bytes;
+        }
+        return readBytes;
+    }
+}
+
 
 /* The read4 API is defined in the parent class Reader4.
       int read4(char[] buf); */
