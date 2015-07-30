0009ca5144306332e3b513a646d275eddb08ad2c
yiyiyayahu <yuanjie_qu@hotmail.com>
2015-07-30 00:08:40 -0700

    Update Reverse Words in a String.java

--- Array-String/Reverse Words in a String.java
+++ Array-String/Reverse Words in a String.java
@@ -38,8 +38,8 @@ public class Solution {
         int j = s.length();
         for(int i = s.length() - 1; i >= 0; i--) {
         	if(s.charAt(i) == ' ') {
-        		j = i;
-        	} else if(i == 0 || s.charAt(i-1) == ' ') {
+                j = i;
+            } else if(i == 0 || s.charAt(i-1) == ' ') {
                 if(sb.length() != 0) sb.append(' ');
                 sb.append(s.substring(i,j));
                 j = i-1;
