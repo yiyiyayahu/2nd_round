97fc9d50e7c16a5028bc4140306a994e9a2135f6
yiyiyayahu <yuanjie_qu@hotmail.com>
2015-07-29 23:09:20 -0700

    Create Populating Next Right Pointers in Each Node II.java

---
+++ Tree/Populating Next Right Pointers in Each Node II.java
@@ -0,0 +1,64 @@
+/*
+Follow up for problem "Populating Next Right Pointers in Each Node".
+
+What if the given tree could be any binary tree? Would your previous solution still work?
+
+Note:
+
+You may only use constant extra space.
+For example,
+Given the following binary tree,
+         1
+       /  \
+      2    3
+     / \    \
+    4   5    7
+After calling your function, the tree should look like:
+         1 -> NULL
+       /  \
+      2 -> 3 -> NULL
+     / \    \
+    4-> 5 -> 7 -> NULL
+*/
+
+
+/*
+好巧妙的做法！！！先是找到下一个的点在哪里，然后先连上
+注意recursive的时候先右边后左边
+*/
+/**
+ * Definition for binary tree with next pointer.
+ * public class TreeLinkNode {
+ *     int val;
+ *     TreeLinkNode left, right, next;
+ *     TreeLinkNode(int x) { val = x; }
+ * }
+ */
+public class Solution {
+    public void connect(TreeLinkNode root) {
+        if(root == null) return;
+  
+        TreeLinkNode p = root.next;
+        while(p != null) {
+            if(p.left != null) {
+                p = p.left;
+                break;
+            }
+            if(p.right != null) {
+                p = p.right;
+                break;
+            }
+            p = p.next;
+        }
+        
+        if(root.right != null) {
+            root.right.next = p;
+        }
+        if(root.left != null) {
+            root.left.next = (root.right != null) ? root.right : p;
+        }
+        
+        connect(root.right);
+        connect(root.left);
+    }
+}
