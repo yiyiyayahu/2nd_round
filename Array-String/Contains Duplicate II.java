db545fd5549d1219f595bd08444cd36cbea3113a
yiyiyayahu <yuanjie_qu@hotmail.com>
2015-08-22 22:45:24 -0700

    Create Contains Duplicate II.java

---
+++ Array-String/Contains Duplicate II.java
@@ -0,0 +1,20 @@
+/*
+Given an array of integers and an integer k, find out whether there are two distinct indices i and j in the array such that 
+nums[i] = nums[j] and the difference between i and j is at most k.
+*/
+
+public class Solution {
+    public boolean containsNearbyDuplicate(int[] nums, int k) {
+        HashMap<Integer, Integer> map = new HashMap<Integer, Integer>();
+        for(int i = 0; i < nums.length; i++) {
+            if(map.containsKey(nums[i])) {
+                int j = map.get(nums[i]);
+                if(i - j <= k) return true;
+                else map.put(nums[i], i);
+            } else {
+                map.put(nums[i], i);
+            }
+        }
+        return false;
+    }
+}
