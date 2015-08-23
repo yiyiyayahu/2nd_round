7c6c666d721df68a61c6614153d8ac42f7dacd2e
yiyiyayahu <yuanjie_qu@hotmail.com>
2015-08-22 21:51:53 -0700

    Update Kth Smallest Element in a BST.java

--- Tree/BST/Kth Smallest Element in a BST.java
+++ Tree/BST/Kth Smallest Element in a BST.java
@@ -24,13 +24,44 @@ The optimal runtime complexity is O(height of BST).
  *     TreeNode(int x) { val = x; }
  * }
  */
+ 
+ /*
+ inorder traversal的第k个就好了
+ */
+ public class Solution {
+    public int kthSmallest(TreeNode root, int k) {
+        List<Integer> list = inorderTraversal(root);
+        return list.get(k-1);
+    }
+    public List<Integer> inorderTraversal(TreeNode root) {
+        List<Integer> lists = new ArrayList<Integer>();
+        if(root == null) return lists;
+        
+        Stack<TreeNode> stack = new Stack<TreeNode>();
+
+        TreeNode n = root;
+        while(!stack.isEmpty() || n != null) {            
+            if(n != null) {
+            	stack.push(n);
+            	n = n.left;
+            } else {
+                n = stack.pop();
+                lists.add(n.val);
+                n = n.right;
+            }
+        }
+        return lists;
+    }
+}
+
+
 public class Solution {
     public int kthSmallest(TreeNode root, int k) {
         PriorityQueue<Integer> queue = new PriorityQueue<Integer>(k, new Comparator<Integer>() {
-			@Override
-			public int compare(Integer o1, Integer o2) {
-				return o2-o1;
-			}
+		@Override
+		public int compare(Integer o1, Integer o2) {
+			return o2-o1;
+		}
         });
         helper(root, queue, k);
         return queue.peek();
@@ -50,3 +81,5 @@ public class Solution {
         helper(root.right, queue, k);
     }
 }
+
+
