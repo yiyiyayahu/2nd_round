e6a852a7182502c94b3032116d339fc678967291
yiyiyayahu <yuanjie_qu@hotmail.com>
2015-08-23 18:20:34 -0700

    Update Merge k Sorted Lists.java

--- LinkedList/Merge k Sorted Lists.java
+++ LinkedList/Merge k Sorted Lists.java
@@ -9,6 +9,47 @@ Merge k sorted linked lists and return it as one sorted list. Analyze and descri
 空间复杂度我貌似用了O(n)? n + n/2 + n/4 + n/8 + ... + 1
 */
 
+
+/*
+还有一种方法是用最小堆做 - peek就是最小的，这样每次往里放，就自动找到最小的了，都帮你sort好了
+时间复杂度还是O(nlogk)
+只是这里一定要自己写个Comparator来比较ListNode
+*/
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int x) { val = x; }
+ * }
+ */
+public class Solution {
+    public ListNode mergeKLists(ListNode[] lists) {
+        if(lists == null || lists.length == 0) return null;
+        PriorityQueue<ListNode> queue = new PriorityQueue<ListNode>(lists.length, new Comparator<ListNode>() {
+            @Override
+            public int compare(ListNode o1, ListNode o2) {
+                return o1.val - o2.val;
+            }
+        });
+        
+        ListNode dummyHead = new ListNode(-1);
+        ListNode tmp = dummyHead;
+        
+        for(ListNode l : lists) {
+            if(l != null) queue.add(l);
+        }
+        
+        while(!queue.isEmpty()) {
+            ListNode n = queue.poll();
+            tmp.next = n;
+            tmp = tmp.next;
+            if(n.next != null) queue.offer(n.next);
+        }
+        return dummyHead.next;
+    }
+}
+
 /**
  * Definition for singly-linked list.
  * public class ListNode {
