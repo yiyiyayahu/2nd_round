93b9f59bda033d2b215fb347475a47535e32a033
yiyiyayahu <yuanjie_qu@hotmail.com>
2015-10-05 14:52:17 -0700

    Update Shortest Word Distance.java

--- Array-String/Shortest Word Distance.java
+++ Array-String/Shortest Word Distance.java
@@ -28,3 +28,37 @@ public class Solution {
         return distance;       
     }
 }
+
+
+/*
+Shortest Word Distance III
+
+This is a follow up of Shortest Word Distance. The only difference is now word1 could be the same as word2.
+
+Given a list of words and two words word1 and word2, return the shortest distance between these two words in the list.
+
+word1 and word2 may be the same and they represent two individual words in the list.
+
+Note:
+You may assume word1 and word2 are both in the list. 
+*/
+public class Solution {
+    public int shortestWordDistance(String[] words, String word1, String word2) {
+        if(words == null || words.length == 0) return -1;
+        int index1 = -1, index2 = -1;
+        int distance = Integer.MAX_VALUE;
+        for(int i = 0; i < words.length; i++) {
+            if(words[i].equals(word1)) {
+                index1 = i;
+                if(index2 != -1 && index1 != index2) 
+                	distance = Math.min(distance, Math.abs(index1-index2));
+            } 
+            if(words[i].equals(word2)) {
+                index2 = i;
+                if(index1 != -1 && index1 != index2) 
+                	distance = Math.min(distance, Math.abs(index1-index2));
+            }
+        }
+        return distance;      
+    }
+}
