9d470d2931d24abf4bf9b7bdd1f537025d27e915
yiyiyayahu <yuanjie_qu@hotmail.com>
2015-10-05 14:27:35 -0700

    Create Shortest Word Distance.java

---
+++ Array-String/Shortest Word Distance.java
@@ -0,0 +1,30 @@
+/*
+Given a list of words and two words word1 and word2, return the shortest distance between these two words in the list.
+
+For example,
+Assume that words = ["practice", "makes", "perfect", "coding", "makes"].
+
+Given word1 = “coding”, word2 = “practice”, return 3.
+Given word1 = "makes", word2 = "coding", return 1.
+
+Note:
+You may assume that word1 does not equal to word2, and word1 and word2 are both in the list. 
+*/
+
+public class Solution {
+    public int shortestDistance(String[] words, String word1, String word2) {
+        if(words == null || words.length == 0) return -1;
+        int index1 = -1, index2 = -1;
+        int distance = Integer.MAX_VALUE;
+        for(int i = 0; i < words.length; i++) {
+            if(words[i].equals(word1)) {
+                index1 = i;
+                if(index2 != -1) distance = Math.min(distance, Math.abs(index1-index2));
+            } else if(words[i].equals(word2)) {
+                index2 = i;
+                if(index1 != -1) distance = Math.min(distance, Math.abs(index1-index2));
+            }
+        }
+        return distance;       
+    }
+}
