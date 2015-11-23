5578f103043883853e51b841f52f0066d2829192
yiyiyayahu <yuanjie_qu@hotmail.com>
2015-11-22 21:42:59 -0800

    Update Flip Game II.java

--- DFS-Backtracking/Flip Game II.java
+++ DFS-Backtracking/Flip Game II.java
@@ -12,6 +12,36 @@ Derive your algorithm's runtime complexity.
 */
 
 /*
+优化版，防止重复计算
+*/
+public class Solution {
+    public boolean canWin(String s) {
+        if(s == null || s.length() <= 1) return false;
+        return dfs(s);
+    }
+    public boolean dfs(String s) {
+        StringBuilder sb = new StringBuilder(s);
+        HashMap<String, Boolean> map = new HashMap<String, Boolean>();
+        
+        for(int i = 0; i < s.length()-1; i++) {
+            if(s.charAt(i) == s.charAt(i+1) && s.charAt(i) == '+') {
+                sb.setCharAt(i, '-');
+                sb.setCharAt(i+1, '-');
+                String str = sb.toString();
+                if(!map.containsKey(str)) {
+                    boolean canWin = dfs(str);
+                    map.put(str, canWin);
+                } 
+                if(!map.get(str)) return true;
+                
+                sb.setCharAt(i, '+');
+                sb.setCharAt(i+1, '+');
+            }
+        }
+        return false;
+    }
+}
+/*
 唉，好久没做题了，果然就没感觉了
 这道题是一道DFS-backtracking的题
 开始我naive的在枚举。。。其实就是如果把++换了，看剩下的字符还能不能赢的问题，就是dfs嘛
