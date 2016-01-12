046a83119e28cc69b63573cbebba97a3f28ba2d6
yiyiyayahu <yuanjie_qu@hotmail.com>
2016-01-11 22:45:13 -0800

    Create Flatten Binary Tree to Linked List.java

---
+++ Tree/Flatten Binary Tree to Linked List.java
@@ -0,0 +1,69 @@
+/*
+Given a binary tree, flatten it to a linked list in-place.
+
+For example,
+Given
+
+         1
+        / \
+       2   5
+      / \   \
+     3   4   6
+The flattened tree should look like:
+   1
+    \
+     2
+      \
+       3
+        \
+         4
+          \
+           5
+            \
+             6
+click to show hints.
+
+Hints:
+If you notice carefully in the flattened tree, each node's right child points to the next node of a pre-order traversal.
+*/
+
+/*
+我开始的想法居然是先遍历一遍，再构造树。。。
+大姐。。。tree咱能想想recursive么。。。
+后来写code出了一个问题，就是left==null的情况没考虑
+*/
+
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+public class Solution {
+    public void flatten(TreeNode root) {
+        flattenNode(root);
+    }
+    public TreeNode flattenNode(TreeNode root) {
+        if(root == null) return root;
+        
+        TreeNode left = flattenNode(root.left);
+        TreeNode right = flattenNode(root.right);
+        
+        root.left = null;
+        if(left == null) {
+            root.right = right;
+            return root;
+        }
+        
+        root.right = left;
+        
+        while(left.right != null) {
+            left = left.right;
+        }
+        left.right = right;
+        return root;
+    }
+}
