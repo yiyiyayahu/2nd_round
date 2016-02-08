739ca91c100c7b126d5ae217ec16c5c1b3196a04
yiyiyayahu <yuanjie_qu@hotmail.com>
2016-02-07 20:10:37 -0800

    Create Group Anagrams.java

---
+++ Array-String/Group Anagrams.java
@@ -0,0 +1,52 @@
+/*
+Given an array of strings, group anagrams together.
+
+For example, given: ["eat", "tea", "tan", "ate", "nat", "bat"], 
+Return:
+
+[
+  ["ate", "eat","tea"],
+  ["nat","tan"],
+  ["bat"]
+]
+Note:
+For the return value, each inner list's elements must follow the lexicographic order.
+All inputs will be in lower-case.
+*/
+
+/*
+对于这两个排序的要求，其实我都是用的java自带的函数，这样真的可以么？
+一个是Arrays.sort()
+还有一个是Collections.sort(list);
+*/
+
+public class Solution {
+    public List<List<String>> groupAnagrams(String[] strs) {
+        List<List<String>> ret = new ArrayList<List<String>>();
+        if(strs.length == 0) return ret;
+        
+        HashMap<String, List<String>> map = new HashMap<String, List<String>>();
+        for(int i = 0; i < strs.length; i++) {
+            String s = strs[i];
+            char[] arr = s.toCharArray();
+            Arrays.sort(arr);
+            String key = String.valueOf(arr);
+            List<String> list;
+            if(map.isEmpty() || !map.containsKey(key) ) {
+                list = new ArrayList<String>();
+                list.add(s);
+                map.put(key, list);
+            } else {
+                list = map.get(key);
+                list.add(s);
+                map.put(key, list);
+            }
+        }
+        for(String k : map.keySet()) {
+            List<String> l = map.get(k);
+            Collections.sort(l);
+            ret.add(l);
+        }
+        return ret;
+    }
+}
