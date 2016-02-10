a7a4ffc3b49d6bc9486946d683f5809e971e2fe9
yiyiyayahu <yuanjie_qu@hotmail.com>
2016-02-09 20:00:53 -0800

    Update Missing Number.java

--- Array-String/Missing Number.java
+++ Array-String/Missing Number.java
@@ -9,6 +9,22 @@ Your algorithm should run in linear runtime complexity. Could you implement it u
 */
 
 /*
+直接算sum就好了嘛。。。
+*/
+public class Solution {
+    public int missingNumber(int[] nums) {
+        if(nums == null || nums.length == 0) return 1;
+        
+        int n = nums.length;
+        int sum = 0;
+        for(int i = 0; i < n; i++) {
+            sum += nums[i];
+        }
+        return n * (n+1)/2 - sum;
+    }
+}
+
+/*
 和First Missing Positive差不多
 */
 
