532cb936e9ef1186e90bde961a807af8aef2911f
yiyiyayahu <yuanjie_qu@hotmail.com>
2016-02-10 20:43:09 -0800

    Create Palindrome Partitioning.java

---
+++ DFS-Backtracking/Palindrome Partitioning.java
@@ -0,0 +1,51 @@
+/*
+Given a string s, partition s such that every substring of the partition is a palindrome.
+
+Return all possible palindrome partitioning of s.
+
+For example, given s = "aab",
+Return
+
+  [
+    ["aa","b"],
+    ["a","a","b"]
+  ]
+*/
+
+public class Solution {
+    public List<List<String>> partition(String s) {
+        List<List<String>> ret = new ArrayList<List<String>>();
+        if(s == null || s.length() == 0) return ret;
+        
+        dfs(s, ret, new ArrayList<String>(), 0);
+        return ret;
+    }
+    
+    public void dfs(String s, List<List<String>> ret, List<String> list, int index) {
+        if(index == s.length()) {
+            ret.add(list);
+            return;
+        }
+        
+        int len = s.length();
+        for(int i = index+1; i <= len; i++) {
+            String s1 = s.substring(index, i);
+            if(isPalindrome(s1)) {
+                List<String> l = new ArrayList<String>(list);
+                l.add(s1);
+                dfs(s, ret, l, i);                
+            }
+        }
+    }
+    
+    public boolean isPalindrome(String s) {
+        int len = s.length();
+        if(len == 1) return true;
+        int start = 0, end = len-1;
+        while(start <= end) {
+            if(s.charAt(start++) == s.charAt(end--)) continue;
+            else return false;
+        }
+        return true;
+    }
+}
