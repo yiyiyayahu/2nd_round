e2f6dd8bbde3205f5b4263c2213f49bc4d67fea5
yiyiyayahu <yuanjie_qu@hotmail.com>
2016-02-11 19:52:43 -0800

    Update Palindrome Partitioning II.java

--- DP/Palindrome Partitioning II.java
+++ DP/Palindrome Partitioning II.java
@@ -13,8 +13,42 @@ dp[i] = min(dp[k-1])+1 if(k到i是palindrome)
         0(if0到k是palindrome)
 果断TLE 哭
 
-哦，其实我的做法还可以，只是每次我都检测一遍是不是回文有点重复了，可以用一个数组, isPal[i][j]来缓存i到j是不是回文
+哦，其实我的做法还可以，只是每次我都检测一遍是不是回文有点重复了，可以用一个数组, isPalin[i][j]来缓存i到j是不是回文
+code写的有点乱。。
 */
+public class Solution {
+    public int minCut(String s) {
+        if(s == null || s.length() == 0) return 0;
+
+        int len = s.length();
+        int[] dp = new int[len+1];
+        dp[0] = 0;
+        boolean[][] isPalin = new boolean[len][len];
+        for(int i = 0; i < len; i++) {
+            int tmp = Integer.MAX_VALUE;
+            for(int k = 0; k <= i; k++) {
+                if(s.charAt(i) == s.charAt(k)) {
+                    if(i-1<k+1 || isPalin[k+1][i-1]) {
+                        isPalin[k][i] = true;
+                        tmp = Math.min(tmp, dp[k]);
+                    }
+                }
+            }
+            if(isPalin[0][i]) dp[i+1] = 0;
+            else dp[i+1] = tmp + 1;
+        }
+        return dp[len];       
+    }
+    
+    public boolean isPalindrome(String s, int start, int end) {
+        while(start <= end) {
+            if(s.charAt(start++) != s.charAt(end--)) return false;
+        }
+        return true;
+    }
+}
+
+
 
 public class Solution {
     public int minCut(String s) {
