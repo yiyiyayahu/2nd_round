297420e0db4974816de841c2c79441e9aaee1924
yiyiyayahu <yuanjie_qu@hotmail.com>
2016-02-16 18:33:49 -0800

    Update First Bad Version.java

--- BinarySearch/First Bad Version.java
+++ BinarySearch/First Bad Version.java
@@ -17,7 +17,22 @@ Implement a function to find the first bad version. You should minimize the numb
 
 /* The isBadVersion API is defined in the parent class VersionControl.
       boolean isBadVersion(int version); */
-      
+public class Solution extends VersionControl {
+    public int firstBadVersion(int n) {
+        if(n <= 0) return -1;
+        int start = 1, end = n;
+        while(start <= end) {
+            int mid = start + (end - start)/2;
+            if(isBadVersion(mid)) {
+                end = mid-1;
+            } else {
+                start = mid+1;
+            }
+        }
+        return start;
+    }
+}
+
 public class Solution extends VersionControl {
     public int firstBadVersion(int n) {
         int start = 1, end = n;
