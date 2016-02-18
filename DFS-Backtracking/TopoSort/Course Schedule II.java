0f1de492dabdc5e290b4478aecda598f8e517a19
yiyiyayahu <yuanjie_qu@hotmail.com>
2016-02-18 00:02:36 -0800

    Update Course Schedule II.java

--- DFS-Backtracking/TopoSort/Course Schedule II.java
+++ DFS-Backtracking/TopoSort/Course Schedule II.java
@@ -20,7 +20,44 @@ The input prerequisites is a graph represented by a list of edges, not adjacency
 
 */
 
-
+/*
+改进了一下，就是不再是算degree从最大的numCourses开始往下面减了
+而是改变了一下指针的方向，edge[0]<-edge[1]，然后小的degree(也就是index)放当前的course number，degree[index++]=course
+*/
+public class Solution {
+    static int index;
+    public int[] findOrder(int numCourses, int[][] prerequisites) {
+        int len = prerequisites.length;
+        int[] degree = new int[numCourses];
+        index = 0;
+        List<List<Integer>> graph = new ArrayList<List<Integer>>();
+        for(int i = 0; i < numCourses; i++) {
+            graph.add(new ArrayList<Integer>());
+        }
+        for(int[] edge : prerequisites) {
+        	graph.get(edge[0]).add(edge[1]);
+        }
+        
+        int[] visited = new int[numCourses];
+        for(int i = 0; i < numCourses; i++) {
+            if(!dfs(graph, i, visited, degree)) return new int[0];
+        }      
+        return degree;
+    }
+    public boolean dfs(List<List<Integer>> graph, int course, int[] visited, int[] degree) {
+        if(visited[course] == 2) return true;
+        if(visited[course] == 1) return false;
+        
+        visited[course] = 1;
+        for(int i = 0; i < graph.get(course).size(); i++) {
+            int next = graph.get(course).get(i);
+            if(!dfs(graph, next, visited, degree)) return false;
+        }
+        visited[course] = 2;
+        degree[index++] = course;
+        return true;
+    }
+}
 /*
 下面的这个解法是错误的，原因是：
 prerequisites可能不是完全的，比如一共有4个course但是可能prerequisite没有，或者只有一个[0,1]这样的
