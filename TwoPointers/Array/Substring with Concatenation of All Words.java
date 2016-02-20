1198f9e01694e1d546c687fdf1e7b40c933635dc
yiyiyayahu <yuanjie_qu@hotmail.com>
2016-02-20 12:19:52 -0800

    Update Substring with Concatenation of All Words.java

--- TwoPointers/Array/Substring with Concatenation of All Words.java
+++ TwoPointers/Array/Substring with Concatenation of All Words.java
@@ -10,53 +10,65 @@ You should return the indices: [0,9].
 */
 
 /*
-这个解法的优化是，与其再重新start++那么从头找，不如看看能跳过几个word那种长度的单词，明儿再想想，好难写啊。。。。
+这个解法优化了一下，思想是这样的：
+1. 首先搜索的起点是吧，i只是从0到words[0].length()，然后剩下的就是其他两个指针的事儿了
+比如aaa这样，然后每个word的length是1的话，i的值只是0和1，就是从第0个开始还是第一个开始
+
+2. 然后用两个指针 start和j，start就是当前查找的starting indices，然后j就从i开始一直找到结尾
+新建一个HashMap tmp来存找过的word，count来track找到的word的总数
+然后j往后移，如果发现，诶，map里不存在诶，说明j+n之前的都否定了，重头来，start=j+n;
+但是如果发现map一直存在的，但是可能当前的word比map里面多了，那这种情况就是start一直后移直到tmp里面的不比map里面的多
+这样一直找一直找，当发现count==m的时候就把start add到list里面去
+
+所以这样下来，时间复杂度是O(m*slen)
+
+但是我原来的那种解法呢，不行了，就start++重新开始
+就是外循环是0 ~ slen-m*n，内循环是0 ~ m*n, 时间复杂度会多很多
 */
 public class Solution {
     public List<Integer> findSubstring(String s, String[] words) {
-        Map<String,Integer> map = new HashMap<String,Integer>();
-        for(String word:words){
-            if(map.containsKey(word)){
-                map.put(word,map.get(word)+1);
-            }else{
-                map.put(word,1);
-            }
+        List<Integer> list = new ArrayList<Integer>();
+        if(words.length == 0) return list;
+        
+        int m = words.length, n = words[0].length(), slen = s.length();
+        if(slen < m*n) return list;
+        
+        Map<String, Integer> map = new HashMap<String, Integer>();
+        for(String w : words) {
+            if(map.containsKey(w)) map.put(w, map.get(w)+1);
+            else map.put(w, 1);
         }
-        int length = words[0].length();
-        List<Integer> result = new ArrayList<Integer>();
-        for(int i=0;i<length;i++){
-            //just start from different point...
-            Map<String,Integer> tmp = new HashMap<String,Integer>();
-            int start = i;
-            int count = 0;
-            for(int j=i;j+length<=s.length();j+=length){
-                String word = s.substring(j,j+length);
-                if(map.containsKey(word)){
-                    tmp.put(word,tmp.getOrDefault(word,0)+1);
-                    count+=1;
-                    while(tmp.get(word)>map.get(word)){ //At most greater than zero..
-                        String removeWord = s.substring(start,start+length);
-                        if(tmp.get(removeWord)>1){
-                            //greater than 0..
-                            tmp.put(removeWord,tmp.get(removeWord)-1);
-                        }else{
-                            tmp.remove(removeWord);
+
+        for(int i = 0; i < n; i++) {
+            Map<String, Integer> tmp = new HashMap<String, Integer>();
+            
+            int start = i, count = 0;
+            for(int j = i; j <= slen-n; j += n) {
+                String str = s.substring(j, j+n);
+                if(map.containsKey(str)) {
+                    if(!tmp.containsKey(str)) tmp.put(str, 1);
+                    else tmp.put(str, tmp.get(str)+1);
+                    count ++;
+                    while(tmp.get(str) > map.get(str)) {
+                        String rmWord = s.substring(start, start+n);
+                        if(tmp.get(rmWord) > 1) {
+                            tmp.put(rmWord, tmp.get(rmWord)-1);
+                        } else {
+                            tmp.remove(rmWord);
                         }
-                        start+=length;
-                        count-=1;
-                        if(removeWord.equals(word))
-                            break;
+                        count --;
+                        start += n;
+                        if(rmWord.equals(str)) break;
                     }
-                    if(count==words.length)
-                        result.add(start);
-                }else{
+                    if(count == m) list.add(start);
+                } else {
                     tmp.clear();
                     count = 0;
-                    start = j+length;
+                    start = j + n;
                 }
             }
         }
-        return result;
+        return list;
     }
 }
 /*
@@ -79,20 +91,14 @@ public class Solution {
         
         int start = 0;
         while(start <= len-m*n) {
-            String curr = s.substring(start, start+m);
-            if(!map.containsKey(curr)) {
-                start ++;
-                continue;
-            }
-            int i = start;
             Map<String, Integer> tmp = new HashMap<String, Integer>(map);
-            while(i < start+m*n) {
+            
+            for(int i = start; i < start + m *n; i += m) {
                 String str = s.substring(i, i+m);
-                if(tmp.isEmpty() || !tmp.containsKey(str)) break;
+                if(!tmp.containsKey(str)) break;
                 
                 if(tmp.get(str) > 1) tmp.put(str, tmp.get(str)-1);
                 else tmp.remove(str);
-                i += m;
             }
             if(tmp.isEmpty()) list.add(start);
             start ++;
