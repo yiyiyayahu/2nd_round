453e08ea1a42d2a869dc570034dd4ee3ccd86d1d
yiyiyayahu <yuanjie_qu@hotmail.com>
2016-03-04 00:02:11 -0800

    Update Power of Two.java

--- bit-manipulation/Power of Two.java
+++ bit-manipulation/Power of Two.java
@@ -7,6 +7,12 @@ Given an integer, write a function to determine if it is a power of two.
 */
 public class Solution {
     public boolean isPowerOfTwo(int n) {
+        return n > 0 && (n & (n-1)) == 0;
+    }
+}
+
+public class Solution {
+    public boolean isPowerOfTwo(int n) {
         if(n <= 0) return false;
         int tmp = n;
         while(tmp != 1) {
