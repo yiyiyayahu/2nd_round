40d4d4d5e1264f84a3e9e6ee7e7bd3dbab9fe658
yiyiyayahu <yuanjie_qu@hotmail.com>
2016-03-05 15:38:23 -0800

    Update Add Two Numbers.java

--- LinkedList/Add Two Numbers.java
+++ LinkedList/Add Two Numbers.java
@@ -42,3 +42,23 @@ public class Solution {
         return ret.next;
     }
 }
+
+public class Solution {
+    public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
+        int carry = 0;
+        ListNode dummy = new ListNode(-1);
+        ListNode tmp = dummy;
+        while(carry != 0 || (l1 != null && l2 != null)) {
+            int val = (l1!=null?l1.val:0) + (l2!=null?l2.val:0) + carry;
+            if(val >= 10) {val = val-10; carry=1;}
+            else carry = 0;
+            tmp.next = new ListNode(val);
+            tmp = tmp.next;
+            if(l1 != null) l1 = l1.next;
+            if(l2 != null) l2 = l2.next;
+        }
+        tmp.next = (l1==null) ? l2 : l1;
+
+        return dummy.next;
+    }
+}
