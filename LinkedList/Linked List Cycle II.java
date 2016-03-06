f0ba7631a560b1954c7deeef4f5a5c1cca1b3c2d
yiyiyayahu <yuanjie_qu@hotmail.com>
2016-03-05 16:30:06 -0800

    Update Linked List Cycle II.java

--- LinkedList/Linked List Cycle II.java
+++ LinkedList/Linked List Cycle II.java
@@ -26,3 +26,24 @@ public class Solution {
         return null;       
     }
 }
+
+
+public class Solution {
+    public ListNode detectCycle(ListNode head) {
+        ListNode slow = head, fast = head;
+        while(fast != null && fast.next != null) {
+            slow = slow.next;
+            fast = fast.next.next;
+            if(slow == fast) {
+                break;
+            }
+        }
+        if(fast == null || fast.next == null) return null;
+        slow = head;
+        while(slow != fast) {
+            slow = slow.next;
+            fast = fast.next;
+        }
+        return slow;
+    }
+}
