2877d786c6322b7d1a923784dff8694756fc996a
yiyiyayahu <yuanjie_qu@hotmail.com>
2016-03-06 18:12:05 -0800

    Update Generate Parentheses.java

--- DFS-Backtracking/Generate Parentheses.java
+++ DFS-Backtracking/Generate Parentheses.java
@@ -6,6 +6,36 @@ For example, given n = 3, a solution set is:
 "((()))", "(()())", "(())()", "()(())", "()()()" 
 */
 
+/*
+用一个char[] arr要高效很多。。。这样就不用每次新建StringBuilder了
+*/
+public class Solution {
+    public List<String> generateParenthesis(int n) {
+        List<String> list = new ArrayList<>();
+        char[] str = new char[n*2];
+        helper(n, n, 0, str, list);
+        return list;
+    }
+    
+    public void helper(int left, int right, int index, char[] str, List<String> list) {
+        if(left < 0 || left > right) return;
+        if(left == 0 && right == 0) {
+            String s = String.copyValueOf(str);
+            list.add(s);
+            return;
+        }
+        
+        if(left > 0) {
+            str[index] = '(';
+            helper(left-1, right, index+1, str, list);
+        }
+        if(right > left) {
+            str[index] = ')';
+            helper(left, right-1, index+1, str, list);
+        }
+    }
+}
+
 public class Solution {
     public List<String> generateParenthesis(int n) {
         List<String> list = new ArrayList<String>();
