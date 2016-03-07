dc3080452d8c9fa3e4a56e069da7fc9691d0d527
yiyiyayahu <yuanjie_qu@hotmail.com>
2016-03-06 20:09:56 -0800

    Update Search a 2D Matrix II.java

--- BinarySearch/Search a 2D Matrix II.java
+++ BinarySearch/Search a 2D Matrix II.java
@@ -55,3 +55,24 @@ public class Solution {
         }
     }
 }
+
+/*
+简单的做法
+*/
+public class Solution {
+    public boolean searchMatrix(int[][] matrix, int target) {
+        if(matrix.length == 0) return false;
+        
+        int i = 0, j = matrix[0].length-1;
+        while(i < matrix.length && j >= 0) {
+            if(matrix[i][j] < target) {
+                i ++;
+            } else if(matrix[i][j] > target) {
+                j --;
+            } else {
+                return true;
+            }
+        }
+        return false;
+    }
+}
