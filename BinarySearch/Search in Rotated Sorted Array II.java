1f491027a0d8302fb586285e719b740ccec327e0
yiyiyayahu <yuanjie_qu@hotmail.com>
2016-03-07 00:22:32 -0800

    Update Search in Rotated Sorted Array II.java

--- BinarySearch/Search in Rotated Sorted Array II.java
+++ BinarySearch/Search in Rotated Sorted Array II.java
@@ -11,6 +11,45 @@ Write a function to determine if a given target is in the array.
 有重复就是可能nums[start] == nums[mid]，找不到pivot，那就只能start一位一位移
 time: O(n) space: O(1)
 */
+
+/*
+有重复的时候leetcode解法很好
+[2,2,2,3,4,2]
+如果发现nums[start] == nums[mid]，可以check一下是否nums[end] == num[mid]，如果不是，只要遍历右边就可以了，不然左右都要遍历
+其实最后那种情况，我觉得可以写个辅助函数，check左边，如果是true的话直接返回就可以了，不然的话再遍历右边。不过只是个简单的优化啦
+*/
+public class Solution {
+    public boolean search(int[] nums, int target) {
+        if(nums == null || nums.length == 0) return false;
+        int start = 0, end = nums.length-1;
+        while(start <= end) {
+            int mid = (start + end)/2;
+            if(nums[mid] == target) return true;
+            
+            if(nums[start] < nums[mid]) {
+            	if(nums[start] <= target && nums[mid] > target) {
+                    end = mid-1;
+                } else {
+                    start = mid + 1;
+                }
+            } else if(nums[start] > nums[mid]) {
+                if(nums[end] >= target && nums[mid] < target) {
+                    start = mid + 1;
+                } else {
+                    end = mid - 1;
+                }
+            } else {
+                if(nums[mid] != nums[end]) {
+                    start = mid+1;
+                } else {
+                    start ++;
+                }
+            }
+        }
+        return false;
+    }
+}
+
 public class Solution {
     public boolean search(int[] nums, int target) {
         if(nums == null || nums.length == 0) return false;
