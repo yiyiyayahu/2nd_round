c47f061d9674dd899ae8c1386c13b69d588377a1
yiyiyayahu <yuanjie_qu@hotmail.com>
2016-03-16 23:07:06 -0700

    Update Number of Islands.java

--- Graph/Union-Find/Number of Islands.java
+++ Graph/Union-Find/Number of Islands.java
@@ -21,6 +21,37 @@ Answer: 3
 */
 
 /*
+DFS的解法，3ms
+就是找到一个1，把它周围的1都设为0，然后接着找下一个1就可以了
+*/
+public class Solution {
+    public int numIslands(char[][] grid) {
+        if(grid.length == 0) return 0;
+        
+        int m = grid.length, n = grid[0].length;
+        int count = 0;
+        for(int i = 0; i < m; i++) {
+            for(int j = 0; j < n; j++) {
+                if(grid[i][j] == '1') {
+                    count ++;
+                    dfs(i, j, grid);
+                }
+            }
+        }
+        return count;
+    }
+    
+    private void dfs(int i, int j, char[][] grid) {
+        if(i < 0 || i >= grid.length || j < 0 || j >= grid[0].length) return;
+        if(grid[i][j] != '1') return;
+        grid[i][j] = '0';
+        dfs(i-1, j, grid);
+        dfs(i+1, j, grid);
+        dfs(i, j-1, grid);
+        dfs(i, j+1, grid);
+    }
+}
+/*
 改进了一下union find的解法，受2的启发 - 13ms，还是很弱啊，看来其他的解法会好一点
 */
 public class Solution {
