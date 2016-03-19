f82fc0de10823fa90d1976e25082499e7a587389
yiyiyayahu <yuanjie_qu@hotmail.com>
2016-03-18 21:40:56 -0700

    Create Binary Tree Upside Down.java

---
+++ Tree/Binary Tree Upside Down.java
@@ -0,0 +1,50 @@
+/*
+Given a binary tree where all the right nodes are either leaf nodes with a sibling (a left node that shares the same parent node) or empty, 
+flip it upside down and turn it into a tree where the original right nodes turned into left leaf nodes. Return the new root.
+
+For example:
+Given a binary tree {1,2,3,4,5},
+    1
+   / \
+  2   3
+ / \
+4   5
+return the root of the binary tree [4,5,2,#,#,3,1].
+   4
+  / \
+ 5   2
+    / \
+   3   1
+*/
+
+/*
+注意NPE，就是root.left==null的时候就可以直接返回了，开始没判断的话有可能tmp是null，这样while(tmp.right!=null)就是NPE
+其次呢，该断掉的link断掉，比如root.left=null, root.right=null
+*/
+
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+public class Solution {
+    public TreeNode upsideDownBinaryTree(TreeNode root) {
+        if(root == null || root.left == null) return root;
+        
+        TreeNode n = root;
+        TreeNode left = upsideDownBinaryTree(root.left);
+        TreeNode right = upsideDownBinaryTree(root.right);
+        root.left = null;
+        root.right = null;
+        
+        TreeNode tmp = left;
+        while(tmp.right != null) tmp = tmp.right;
+        tmp.left = right;
+        tmp.right = root;
+        return left;
+    }
+}
