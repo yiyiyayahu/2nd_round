a7a177a7543c9f6909e19bbb6c488bd1f9c76b18
yiyiyayahu <yuanjie_qu@hotmail.com>
2016-03-20 22:29:59 -0700

    Update Search Insert Position.java

--- BinarySearch/Search Insert Position.java
+++ BinarySearch/Search Insert Position.java
@@ -17,15 +17,17 @@ time: O(logn) space:O(1)
 public class Solution {
     public int searchInsert(int[] nums, int target) {
         int start = 0, end = nums.length-1;
-        while(start < end) {
+        while(start <= end) {
             int mid = (start + end)/2;
             if(nums[mid] < target) {
                 start = mid + 1;
+            } else if(nums[mid] == target) {
+                return mid;
             } else {
-                end = mid;
+                end = mid - 1;
             }
         }
-        return (nums[start]<target)? start+1 : start;
+        return start;
     }
 }
 
