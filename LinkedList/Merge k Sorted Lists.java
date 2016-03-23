2290ff17ec614fef1ab0c7877fc6021d75dd5a51
yiyiyayahu <yuanjie_qu@hotmail.com>
2016-03-22 23:41:26 -0700

    Update Merge k Sorted Lists.java

--- LinkedList/Merge k Sorted Lists.java
+++ LinkedList/Merge k Sorted Lists.java
@@ -3,16 +3,46 @@ Merge k sorted linked lists and return it as one sorted list. Analyze and descri
 */
 
 /*
-这里我总是重复分配空间会不会不太好？可以直接利用原来的lists？比如0和最后一个放到0里面，一次类推？
+这里我总是重复分配空间会不会不太好？可以直接利用原来的lists？比如0和最后一个放到0里面 - 嗯呐，对の
 时间复杂度应该是：如果lists数组的长度是k，每个ListNode的长度是n
 一共重复logk次，每次遍历merge两个list的话大概是O(n)所以是O(nlogk)
-空间复杂度我貌似用了O(n)? n + n/2 + n/4 + n/8 + ... + 1
+空间不用算重新分配的list的space，所以就是O(1)
 */
-
-
+public class Solution {
+    public ListNode mergeKLists(ListNode[] lists) {
+        if(lists.length == 0) return null;
+        int end = lists.length-1;
+        while(end > 0) {
+            int start = 0;
+            while(start < end) {
+                ListNode l1 = lists[start], l2 = lists[end];
+                lists[start] = mergeTwoLists(l1, l2);
+                start ++;
+                end --;
+            }
+        }
+        return lists[0];
+    }
+    private ListNode mergeTwoLists(ListNode l1, ListNode l2) {
+        ListNode dummyHead = new ListNode(-1);
+        ListNode p = dummyHead;
+        while(l1 != null && l2 != null) {
+            if(l1.val < l2.val) {
+                p.next = l1;
+                l1 = l1.next;
+            } else {
+                p.next = l2;
+                l2 = l2.next;
+            }
+            p = p.next;
+        }
+        p.next = (l1==null) ? l2 : l1;
+        return dummyHead.next;
+    }
+}
 /*
 还有一种方法是用最小堆做 - peek就是最小的，这样每次往里放，就自动找到最小的了，都帮你sort好了
-时间复杂度还是O(nlogk)
+时间复杂度还是O(nlogk) 空间是O(k)
 只是这里一定要自己写个Comparator来比较ListNode
 */
 /**
