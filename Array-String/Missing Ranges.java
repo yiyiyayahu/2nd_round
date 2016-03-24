88fd83dc916fedee6eed54f9e42671913187a8be
yiyiyayahu <yuanjie_qu@hotmail.com>
2016-03-23 21:43:46 -0700

    Update Missing Ranges.java

--- Array-String/Missing Ranges.java
+++ Array-String/Missing Ranges.java
@@ -3,6 +3,29 @@ Given a sorted integer array where the range of elements are [lower, upper] incl
 
 For example, given [0, 1, 3, 50, 75], lower = 0 and upper = 99, return ["2", "4->49", "51->74", "76->99"].
 */
+
+/*
+依旧是leetcode那本书上的简洁写法
+就是first和last element比较不好处理，所以就加了一个lower-1，upper+1的这么一个fake range，if else就会少很多
+*/
+public class Solution {
+    public List<String> findMissingRanges(int[] nums, int lower, int upper) {
+        List<String> ranges = new ArrayList<>();
+        int prev = lower-1;
+        for(int i = 0; i <= nums.length; i++) {
+            int curr = (i==nums.length) ? upper+1 : nums[i];
+            if(curr-prev >= 2) {
+                ranges.add(getRange(prev+1,curr-1));
+            }
+            prev = curr;
+        }
+        return ranges;
+    }
+    private String getRange(int from, int to) {
+        return from==to? String.valueOf(from) : from + "->" + to;
+    }
+}
+
 public class Solution {
     public List<String> findMissingRanges(int[] nums, int lower, int upper) {
         List<String> lists = new ArrayList<String>();
