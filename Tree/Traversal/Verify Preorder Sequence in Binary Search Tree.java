f1d4cbdc1c436ee191a379c41d42642a0b48f75b
yiyiyayahu <yuanjie_qu@hotmail.com>
2016-03-25 14:04:04 -0700

    Update Verify Preorder Sequence in Binary Search Tree.java

--- Tree/Traversal/Verify Preorder Sequence in Binary Search Tree.java
+++ Tree/Traversal/Verify Preorder Sequence in Binary Search Tree.java
@@ -8,6 +8,33 @@ Could you do it using only constant space complexity?
 */
 
 /*
+有了下面那个TLE的解法之后现在思路就清晰多了
+一开始拿到这道题的时候有些懵，总想着构造这样一个tree，实际上没有必要
+考虑一下不太行的情况，无非就是[9,8,10,5,12]这种，从10开始已经应该是比9大的了，但是遇到5就应该是invalid preorder了
+所以呢，用一个stack，遇到9push，遇到小于peek的push，遇到大的就把stack里面小于它的pop出来，并且记录最后pop的那个值，也就是root的值，这里是9
+然后接着往后走，如果发现有一个比root小的，就return false
+这样时间就是O(n) 但是空间还是不太好，因为用了一个stack，所以space是O(n)
+再想想怎么优化
+*/
+public class Solution {
+    public boolean verifyPreorder(int[] preorder) {
+        if(preorder.length < 2) return true;
+        
+        Stack<Integer> stack = new Stack<Integer>();
+        int min = Integer.MIN_VALUE;
+        for(int i = 0; i < preorder.length; i++) {
+            int val = preorder[i];
+            if(val < min) return false;
+            
+            while(!stack.isEmpty() && val > stack.peek()) {
+                min = stack.pop();
+            }
+            stack.push(val);
+        }
+        return true;
+    }
+}
+/*
 我这个做法不好，已经TLE了，好多重复计算，时间复杂度O(n^2)
 思路是这样的preorder[i]肯定是当前的root value了，然后从i+1开始往后找，所有开始小于这个root value的都是left subtree，
 看到一个大于它的就说明right subtree开始了，但是如果在right这边发现有一个比root value小，那就return false了，不然就recursive的去找
