e2c5763c681c93c8edf457b5f04e1e4c54b04d11
yiyiyayahu <yuanjie_qu@hotmail.com>
2016-03-27 15:57:14 -0700

    Update Binary Tree Longest Consecutive Sequence.java

--- Tree/Binary Tree Longest Consecutive Sequence.java
+++ Tree/Binary Tree Longest Consecutive Sequence.java
@@ -79,3 +79,19 @@ public class Solution {
         return longest;
     }
 }
+
+
+//人家这个写法简洁好多诶。。。这个算是从上往下count，root的count是1，如果root.left
+public class Solution {
+    public int longestConsecutive(TreeNode root) {
+        return (root==null)?0:Math.max(dfs(root.left, 1, root.val), dfs(root.right, 1, root.val));
+    }
+
+    public int dfs(TreeNode root, int count, int val){
+        if(root==null) return count;
+        count = (root.val - val == 1)?count+1:1;
+        int left = dfs(root.left, count, root.val);
+        int right = dfs(root.right, count, root.val);
+        return Math.max(Math.max(left, right), count);
+    }
+}
