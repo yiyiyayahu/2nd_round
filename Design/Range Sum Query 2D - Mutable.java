591335b261dccec732be08200c6d2b1a70e233c5
yiyiyayahu <yuanjie_qu@hotmail.com>
2016-03-28 21:00:46 -0700

    Update Range Sum Query 2D - Mutable.java

--- Design/Range Sum Query 2D - Mutable.java
+++ Design/Range Sum Query 2D - Mutable.java
@@ -69,9 +69,70 @@ public class NumMatrix {
     }
 }
 
-
 // Your NumMatrix object will be instantiated and called as such:
 // NumMatrix numMatrix = new NumMatrix(matrix);
 // numMatrix.sumRegion(0, 1, 2, 3);
 // numMatrix.update(1, 1, 10);
 // numMatrix.sumRegion(1, 2, 3, 4);
+
+
+/*
+用binary indexed tree的话，我只是一层一层的用，所以update和query的时间复杂度变为O(mlogn)，但是从leetcode的时间上来看，并没有改善反而慢了，可能是构造的时间比较久？
+*/
+public class NumMatrix {
+    int[][] tree;
+    int[][] matrix;
+    
+    public NumMatrix(int[][] matrix) {
+        if(matrix.length == 0) return;
+        int m = matrix.length, n = matrix[0].length;
+        this.matrix = matrix;
+        tree = new int[m][n+1];
+        
+        for(int i = 0; i < m; i++) {
+            for(int j = 0; j < n; j++) {
+                updateTree(i, j, matrix[i][j]);
+            }
+        }
+    }
+
+    public void update(int row, int col, int val) {
+        updateTree(row, col, val-matrix[row][col]);
+        matrix[row][col] = val;
+    }
+
+    public int sumRegion(int row1, int col1, int row2, int col2) {
+        int total = 0;
+        for(int i = row1; i <= row2; i++) {
+            if(col1 == 0) total += getSum(i, col2);
+            else total += getSum(i, col2) - getSum(i, col1-1);
+        }
+        return total;
+    }
+    
+    private void updateTree(int i, int j, int val) {
+        int index = j+1;
+        while(index < tree[0].length) {
+            tree[i][index] += val;
+            index = getNext(index);
+        }
+    }
+    
+    private int getSum(int i, int j) {
+        int index = j+1;
+        int sum = 0;
+        while(index > 0) {
+            sum += tree[i][index];
+            index = getParent(index);
+        }
+        return sum;
+    }
+    
+    private int getParent(int index) {
+        return index - (index&-index);
+    }
+    
+    private int getNext(int index) {
+        return index + (index&-index);
+    }
+}
