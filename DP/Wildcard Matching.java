fee646913d9a84dd70cd821517fb1ba6bf11c41c
yiyiyayahu <yuanjie_qu@hotmail.com>
2016-04-02 16:55:14 -0700

    Update Wildcard Matching.java

--- DP/Wildcard Matching.java
+++ DP/Wildcard Matching.java
@@ -18,7 +18,38 @@ isMatch("aab", "c*a*b") → false
 */
 
 /*
-在昨儿晚上做出regular expression matching之后，可算是把这道题做出来了，而且现在觉得不是很难了，好凯西
+自己的做法因为遇到*的时候，循环里面套了个循环，时间复杂度不太好 658ms
+下面是有人的改进算法，从后往前做的，我还没有特别想明白。。。 84ms
+差距好大 T.T
+*/
+public class Solution {
+    public boolean isMatch(String s, String p) {
+        int slen = s.length(), plen = p.length();
+        if(plen == 0) return slen == 0;
+        boolean[][] dp = new boolean[slen+1][plen+1];
+        
+        dp[slen][plen] = true;
+        for(int j = plen-1; j >= 0; j--) {
+            if(p.charAt(j) == '*') dp[slen][j] = true;
+            else break;
+        }
+
+        for(int i = slen-1; i >= 0; i--) {
+            for(int j = plen-1; j >= 0; j--) {
+                char cs = s.charAt(i), cp = p.charAt(j);
+                if(cs == cp || cp == '?') {
+                    dp[i][j] = dp[i+1][j+1];
+                } else if(cp == '*') {
+                    dp[i][j] = dp[i+1][j] || dp[i][j+1];
+                }
+            }
+        }
+
+        return dp[0][0];
+    }
+}
+/*
+在昨儿晚上做出regular expression matching之后，可算是把这道题做出来了，而且现在觉得不是很难了，好开心
 其实如果说用dp做的话，还是regular expression matching比较难
 这个应该还可以缩略一点，就是二维数组压缩成一维，一会儿再想想好啦
 开心~~~
