019c10b145c41fc0fc9e075af21ce57f62eb84c2
yiyiyayahu <yuanjie_qu@hotmail.com>
2016-04-02 17:44:35 -0700

    Update Reverse Linked List.java

--- LinkedList/Reverse Linked List.java
+++ LinkedList/Reverse Linked List.java
@@ -22,6 +22,23 @@ public class Solution {
     }
 }
 
+/*
+这个recursive的方法简洁一些
+比如1->2->3->4
+head.next -> node 2
+ListNode p = reverseList(head.next); p是list的头: 4->3->2
+这时候head.next还是2，只不过变成了尾巴，所以可以直接把head甩过去，head.next.next=head, head.next=null list就变成4->3->2->1
+*/
+public class Solution {
+    public ListNode reverseList(ListNode head) {
+        if(head == null || head.next == null) return head;
+        
+        ListNode p = reverseList(head.next);
+        head.next.next = head;
+        head.next = null;
+        return p;
+    }
+}
 
 public class Solution {
     public ListNode reverseList(ListNode head) {
