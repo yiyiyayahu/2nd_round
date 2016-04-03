58dda2c8d68e3e3b8a7de682fc44f50aebc6e26b
yiyiyayahu <yuanjie_qu@hotmail.com>
2016-04-02 20:35:28 -0700

    Update Letter Combinations of a Phone Number.java

--- DFS-Backtracking/Letter Combinations of a Phone Number.java
+++ DFS-Backtracking/Letter Combinations of a Phone Number.java
@@ -16,6 +16,32 @@ Although the above answer is in lexicographical order, your answer could be in a
 time: O(3^n) space: O(n)
 用一个String[]来存dict还是很巧妙的
 */
+
+public class Solution {
+    static String[] numToLetters = {"","","abc","def","ghi","jkl","mno","pqrs","tuv","wxyz"};
+    
+    public List<String> letterCombinations(String digits) {
+        List<String> list = new ArrayList<>();
+        if(digits.length() == 0) return list;
+        dfs(digits, 0, new StringBuilder(), list);
+        return list;
+    }
+    
+    public void dfs(String digits, int index, StringBuilder sb, List<String> list) {
+        if(index == digits.length()) {
+            list.add(sb.toString());
+            return;
+        }
+        String letters = numToLetters[digits.charAt(index)-'0'];
+        for(int i = 0; i < letters.length(); i++) {
+            char c = letters.charAt(i);
+            sb.append(c);
+            dfs(digits, index+1, sb, list);
+            sb.deleteCharAt(sb.length()-1);
+        }
+    }
+}
+
 public class Solution {
     private String[] dict = {" ", " ", "abc", "def", "ghi", "jkl", "mno", "pqrs", "tuv", "wxyz"};
     
@@ -35,7 +61,7 @@ public class Solution {
         int num = digits.charAt(step) - '0';
         String word = dict[num];
         for(int i = 0; i < word.length(); i++) {
-        	char c = word.charAt(i);
+            char c = word.charAt(i);
             helper(digits, str+c, result, step+1);
         }
     }
