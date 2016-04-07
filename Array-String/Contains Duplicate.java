60223da1b278efa4b6b187f6b371de8c0bd3d49d
yiyiyayahu <yuanjie_qu@hotmail.com>
2016-04-06 20:17:03 -0700

    Update Contains Duplicate.java

--- Array-String/Contains Duplicate.java
+++ Array-String/Contains Duplicate.java
@@ -14,3 +14,14 @@ public class Solution {
         return false;
     }
 }
+
+public class Solution {
+    public boolean containsDuplicate(int[] nums) {
+        Arrays.sort(nums);
+        for (int i = 1; i < nums.length; i++) {
+            if (nums[i] == nums[i-1])
+                return true;
+        }
+        return false;
+    }
+}
