cd2301220080c5a293c91c0f595b9bbb24e7b8c3
yiyiyayahu <yuanjie_qu@hotmail.com>
2016-04-16 11:42:46 -0700

    Update Number of Digit One.java

--- Math/Number of Digit One.java
+++ Math/Number of Digit One.java
@@ -11,6 +11,29 @@ Beware of overflow.
 */
 
 /*
+multiplier makes the digit two parts
+314152
+if m = 1000, then it breaks into 314, 152, and to count the number of digit one in thousands, that would be
+(1+31)*1000=32000 (4 is larger than 1, and thus, for every 0-31, there would be 1000-1999's ones in thousands)
+if m = 100, then it breaks into 3141, 52, and to count the number of digit one in hundreds, that would be
+314*100 + 53 (1 equals to 1, thus, for every 0-313, there would be 100-199, however for 314, there would only be 100-152, in total 53)
+
+remember to do (a/10)*m or (a/10+1)*m, not directly a*m...
+*/
+public class Solution {
+    public int countDigitOne(int n) {
+        int count = 0;
+        for(long m = 1; m <= n; m*=10) {
+            long a = n/m, b = n%m;
+            if(a%10 > 1) count += (a/10+1)*m;
+            else if(a%10 == 1) count += a/10*m + (b+1);
+            else count += a/10*m;
+        }    
+        return count;
+    }
+}
+
+/*
 https://leetcode.com/discuss/44281/8-lines-o-log-n-c-java-python
 http://www.cnblogs.com/grandyang/p/4629032.html
 
@@ -18,13 +41,7 @@ http://www.cnblogs.com/grandyang/p/4629032.html
 比如56，十位上有(5+1)个，个位上也就是10-19有10个，6+10=16
 可以用(x+8)/10来判断一个数是否大于等于2。
 
-举个例子54215，比如现在求百位上的1（即len是100），54215的百位上是2。看到xx100到xx199的百位上都是1， 
-这里xx从0到54，即100->199, 1100->1199...54100->54199, 这些数的百位都是1，百位上的1总数是55*10
-
-如果n是54125,这时由于它的百位是1，先看xx100到xx199，其中xx是0到53，即54*10, 然后看54100到54125，这是26个。
-所以百位上的1的总数是54*10 + 26
-
-如果n是54025，那么只需要看xx100到xx199中百位上的1，这里xx从0到53，总数为54*10
+我之前的理解完全是错误的，什么乱七八糟的，删掉了
 */
 
 public class Solution {
