38b2510120924f544e75b0ca20a4a427fa79d056
yiyiyayahu <yuanjie_qu@hotmail.com>
2016-04-16 15:14:11 -0700

    Update Reverse Bits.java

--- bit-manipulation/Reverse Bits.java
+++ bit-manipulation/Reverse Bits.java
@@ -35,3 +35,21 @@ public class Solution {
         return result;
     }
 }
+
+/*
+这个是一位一位的移
+*/
+public class Solution {
+    // you need treat n as an unsigned value
+    public int reverseBits(int n) {
+        if(n == 0) return 0;
+        
+        int result = 0;
+        for(int i = 0; i < 32; i++) {
+            result <<= 1;
+            if((n&1) == 1) result ++;
+            n >>= 1;
+        }
+        return result;
+    }
+}
