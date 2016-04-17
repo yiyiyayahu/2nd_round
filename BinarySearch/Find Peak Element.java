707a5671814c1cb9d56dd040d6fff08455fe53cd
yiyiyayahu <yuanjie_qu@hotmail.com>
2016-04-17 16:34:39 -0700

    Update Find Peak Element.java

--- BinarySearch/Find Peak Element.java
+++ BinarySearch/Find Peak Element.java
@@ -16,6 +16,18 @@ Your solution should be in logarithmic complexity.
 */
 
 /*
+https://leetcode.com/discuss/68999/java-solution-and-explanation-using-invariants
+这道题其实一直没太明白为什么nums[mid]<nums[mid-1]，可以证明start to mid-1 has a peak
+这个post解释的很详细
+如果这个不等式成立的话
+nums[left]>nums[left-1] && nums[right]>nums[right+1]
+那么从left到right一定有个peak
+1）如果一直是递减的，left是peak
+2）如果一直是递增的，right是peak
+3）如果先递增再递减，那么中间一定有个peak
+所以，如果按照这个思路nums[mid+1]>nums[mid] && nums[right]>nums[right+1] 满足这个条件，所以mid+1,right中间一定有个peak
+nums[left]>nums[left-1] && nums[mid-1]>nums[mid] start,mid-1中间一定有个peak
+
 开始还是没想到思路，如果是O(logn)的话肯定是binary search，但是如何做呢
 其实就是如果nums[mid]<nums[mid-1]的话就在start,mid-1之中肯定有个值。反之，如果nums[mid]<nums[mid+1], mid+1,end之间肯定有个peak
 */
