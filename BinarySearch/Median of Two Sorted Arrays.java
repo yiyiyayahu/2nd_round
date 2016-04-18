d4a34f8608e032c1c0c6636e12492c02d98550a3
yiyiyayahu <yuanjie_qu@hotmail.com>
2016-04-17 18:09:06 -0700

    Update Median of Two Sorted Arrays.java

--- BinarySearch/Median of Two Sorted Arrays.java
+++ BinarySearch/Median of Two Sorted Arrays.java
@@ -3,6 +3,39 @@ There are two sorted arrays nums1 and nums2 of size m and n respectively. Find t
 The overall run time complexity should be O(log (m+n)).
 */
 
+/*
+if (aMid < bMid) Keep [aRight + bLeft]
+
+else Keep [bRight + aLeft]
+
+*/
+public class Solution {
+    public double findMedianSortedArrays(int[] nums1, int[] nums2) {
+        int m = nums1.length, n = nums2.length;
+        if( (m+n)%2 != 0) {
+            return (double)getKth(nums1, 0, nums2, 0, (m+n)/2+1);
+        } else {
+            return (getKth(nums1, 0, nums2, 0, (m+n)/2) + getKth(nums1, 0, nums2, 0, (m+n)/2+1))/2.0;
+        }        
+    }
+    
+    public double getKth(int[] nums1, int astart, int[] nums2, int bstart, int k) {
+        int m = nums1.length, n = nums2.length;
+        if(astart > m-1) return nums2[bstart+k-1];
+        if(bstart > n-1) return nums1[astart+k-1];
+        if(k == 1) return Math.min(nums1[astart], nums2[bstart]);
+        
+        int amid = Integer.MAX_VALUE, bmid = Integer.MAX_VALUE;
+        if(astart+k/2-1 < m) amid = nums1[astart+k/2-1];
+        if(bstart+k/2-1 < n) bmid = nums2[bstart+k/2-1];
+        if(amid < bmid) {
+            return getKth(nums1, astart + k/2, nums2, bstart, k-k/2);
+        } else {
+            return getKth(nums1, astart, nums2, bstart+k/2, k-k/2);
+        }
+    }
+}
+
 public class Solution {
     public double findMedianSortedArrays(int[] nums1, int[] nums2) {
         int m = nums1.length, n = nums2.length;
