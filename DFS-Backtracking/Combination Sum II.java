975a24fe45dfec558fddc8e4e7c580d82dd07300
yiyiyayahu <yuanjie_qu@hotmail.com>
2016-04-17 22:26:28 -0700

    Update Combination Sum II.java

--- DFS-Backtracking/Combination Sum II.java
+++ DFS-Backtracking/Combination Sum II.java
@@ -18,10 +18,43 @@ A solution set is:
 
 /*
 while(i < candidates.length-1 && candidates[i+1] == candidates[i]) i++; 这个是为了去重！！！木有想粗来
-意思就是[1,1,2,5,6,7]
-从index=0开始的1，然后向后recursive [1,1,....]，所以不能像I一样在之前就去重
+意思就是[1,1,2,5,6,7], 从index=0开始的1，然后向后recursive [1,1,....]
 但是这个时候，从index=1开始的1就没必要了，所以要去掉
+
+其实可以在前面去掉也可以在后面去掉，无非就是第二个1没有必要再count了
+在前面去掉就是for loop里面先判断
+if(i != curr && candidates[i] == candidates[i-1]) continue;
+
+在后面去掉就是当前的i做完之后，在for循环最后加一句
+while(i < candidates.length-1 && candidates[i+1] == candidates[i]) i++;
+
+我觉得第一种貌似更好理解，但是要注意i != curr
 */
+
+public class Solution {
+    public List<List<Integer>> combinationSum2(int[] candidates, int target) {
+        List<List<Integer>> ret = new ArrayList<List<Integer>>();
+        Arrays.sort(candidates);
+        helper(candidates, 0, target, new ArrayList<Integer>(), ret);
+        return ret;
+    }
+    
+    public void helper(int[] candidates, int curr, int target, List<Integer> list, List<List<Integer>> ret) {
+        if(target == 0) {
+            ret.add(new ArrayList<Integer>(list));
+            return;
+        }
+        int len = candidates.length;
+        for(int i = curr; i < len; i++) {
+            if(i != curr && candidates[i] == candidates[i-1]) continue;
+            int newTarget = target-candidates[i];
+            if(newTarget < 0) break;
+            list.add(candidates[i]);
+            helper(candidates, i+1, newTarget, list, ret);
+            list.remove(list.size()-1);
+        }
+    }
+}
 
 public class Solution {
     public List<List<Integer>> combinationSum2(int[] candidates, int target) {
