b51d2dc8862c9bc826a83c1973eac71f973a0896
yiyiyayahu <yuanjie_qu@hotmail.com>
2016-04-17 22:19:28 -0700

    Update Subsets II.java

--- DFS-Backtracking/Subsets II.java
+++ DFS-Backtracking/Subsets II.java
@@ -17,6 +17,27 @@ If nums = [1,2,2], a solution is:
 ]
 */
 
+public class Solution {
+    public List<List<Integer>> subsetsWithDup(int[] nums) {
+        Arrays.sort(nums);
+        List<List<Integer>> ret = new ArrayList<List<Integer>>();
+        List<Integer> list = new ArrayList<Integer>();
+        ret.add(list);
+        helper(nums, 0, list, ret);
+        return ret;
+    }
+    
+    public void helper(int[] nums, int curr, List<Integer> list, List<List<Integer>> ret) {
+        if(curr == nums.length) return;
+        for(int i = curr; i < nums.length; i++) {
+            if(i != curr && nums[i] == nums[i-1]) continue;
+            list.add(nums[i]);
+            ret.add(new ArrayList<Integer>(list));
+            helper(nums, i+1, list, ret);
+            list.remove(list.size()-1);
+        }
+    }
+}
 /*
 这个去重其实就是比如1，2，2 弄完第一个2之后我第二个2就可以不用再考虑了
 比如1 -> [1], [1,2] 这个时候就不用remove 2再加第二个2了，直接到[1,2,2]
