a117a3d6c419183c49d6dfd085bc1a5e4aa116eb
yiyiyayahu <yuanjie_qu@hotmail.com>
2016-04-17 23:24:23 -0700

    Update N-Queens II.java

--- DFS-Backtracking/N-Queens II.java
+++ DFS-Backtracking/N-Queens II.java
@@ -6,6 +6,49 @@ Now, instead outputting board configurations, return the total number of distinc
 */
 
 /*
+可以压缩为一维array，每次都是填一行的值
+比如helper(arr, 0, n)就是填arr[0]，填的值代表第0行把Q放在第几列 [0,n-1]中取一个valid的值
+开始可能arr[0]=0，然后index->1 填arr[1]的时候发现不能填0，1只能填2或者3，如果填2的话呢是走不下去的，这时候就会返回上一级，然后填3
+[-1,-1,-1,-1] -> [0,-1,-1,-1] -> [0,2,-1,-1] -> failed
+                              -> [0,3,-1,-1] -> [0,3,1,-1] -> [0,3,1,2]
+这个前面是来检测不在对角线上，后面是检测不在同一列上，在同一行的情况不会出现，因为每一行就填一个值
+if(Math.abs(index-j) == Math.abs(i-arr[j]) || arr[j] == i)
+*/
+public class Solution {
+    static int count;
+    public int totalNQueens(int n) {
+        int[] arr = new int[n];
+        Arrays.fill(arr, -1);
+        count = 0;
+        helper(arr, 0, n);
+        return count;
+    }
+
+    public void helper(int[] arr, int index, int n) {
+        if(index == n) {
+            count ++;
+            return;
+        }
+        
+        for(int i = 0; i < n; i++) {
+            boolean isValid = true;
+            for(int j = 0; j < index; j++) {
+                if(arr[j] == -1) continue;
+                if(Math.abs(index-j) == Math.abs(i-arr[j]) || arr[j] == i) {
+                    isValid = false;
+                    break;
+                }
+            }
+            if(isValid) {
+                arr[index] = i;
+                helper(arr, index+1, n);
+                arr[index] = -1;
+            }
+        }
+    }
+}
+
+/*
 这道题现在做起来好多了，没有开始那么无措
 开始没过是因为以为只考虑前后1位就行了，但是不能同一个对角线的话要把其他的非0点都考虑进去
 */
